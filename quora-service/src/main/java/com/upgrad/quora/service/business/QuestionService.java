f1a42cfbf2354b431e3701ba29dec5f4660ed063
Vikram Ramesh <vicky1988@gmail.com>
2020-12-07 20:36:56 +0530

    Providing for QuestionAPI Endpoint development

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -17,36 +17,4 @@ public class QuestionService {
     QuestionRepository questionRepository;
 
 
-    @Transactional(propagation = Propagation.REQUIRED)
-    public void createQuestion(Question question) {
-        questionRepository.createQuestion(question);
     }
-
-    public List<Question> getAllQuestions() {
-        return questionRepository.getAllQuestions();
-    }
-
-    public Question getQuestionById(String uuid) {
-        Question question = questionRepository.getQuestionById(uuid);
-
-        return question;
-    }
-
-
-    @Transactional(propagation = Propagation.REQUIRED)
-    public Question updateQuestion(Question question) {
-
-        return questionRepository.updateQuestion(question);
-    }
-
-
-    @Transactional(propagation = Propagation.REQUIRED)
-    public void deleteQuestion(Integer id) {
-
-        questionRepository.deleteQuestion(id);
-    }
-
-    public List<Question> getAllQuestionsByUser(User user) {
-        return questionRepository.getAllQuestionsByUser(user);
-    }
-}
